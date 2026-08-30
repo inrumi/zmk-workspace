e9016deca8a1e8e021ba57dd66cfedfa717e3f4c
Johnn Mis <inrumi@gmail.com>
2026-08-30 20:52:23 +0200

    Everything is working right now with the crosses board config locally installed instead of zmk-imported

---
+++ config/boards/gggw/crosses_v2/pinmux.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2026 Vincent Franco
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/sys_io.h>
+
+static int pinmux_nrfmicro_init(void) {
+  const struct device *p0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+  return 0;
+}
+
+SYS_INIT(pinmux_nrfmicro_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
